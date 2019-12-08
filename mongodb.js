8fcf769a5111c8bae0eabeede9d2ad2555909409
Ajay <ajaytechworkshop@gmail.com>
2019-12-08 23:12:46 +0100

    Modified mongo string

--- mongodb.js
+++ mongodb.js
@@ -2,8 +2,8 @@ const mongodb = require('mongodb');
 const MongoClient = mongodb.MongoClient;
 
 //Database Connection URL
-//const connectionUrl = 'mongodb+srv://mongouser:mongouser@mongoone-lz8dp.mongodb.net/test?retryWrites=true&w=majority';
-const connectionUrl = 'mongodb://127.0.0.1:27017';
+const connectionUrl = 'mongodb+srv://tasksadmin:tasksadmin@mongoone-lz8dp.mongodb.net/test?retryWrites=true&w=majority';
+//const connectionUrl = 'mongodb://127.0.0.1:27017';
 const databaseName = 'tasksdb';
 
 //Connect to Mongo DB
--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
 //Database Connection URL
-//const connectionUrl = 'mongodb+srv://mongouser:mongouser@mongoone-lz8dp.mongodb.net/test?retryWrites=true&w=majority';
-const connectionUrl = 'mongodb://127.0.0.1:27017';
+const connectionUrl = 'mongodb+srv://mongouser:mongouser@mongoone-lz8dp.mongodb.net/test?retryWrites=true&w=majority';
+//const connectionUrl = 'mongodb://127.0.0.1:27017';
 const databaseName = 'tasksdb';
 
 mongoose.connect(connectionUrl+'/'+databaseName,{
